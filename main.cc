1726cac65dd5d49f5a5d6b9f71f82d017f2f7afd
Armando Montanez <montanez.armando.l@gmail.com>
2024-11-03 13:38:58 -0800

    Initial multi-animation support

--- main.cc
+++ main.cc
@@ -8,9 +8,9 @@
 #include "touch_wheel.h"
 #include "hardware/i2c.h"
 
-class PetalAnimation : public Animation {
+class PetalAnimation1 : public Animation {
  public:
-  PetalAnimation(I2cBus& bus, uint32_t frame_timing_us) : Animation(frame_timing_us), pm_(bus) {}
+  PetalAnimation1(I2cBus& bus) : pm_(bus) {}
   void Init() {
     pm_.Init();
   }
@@ -43,6 +43,41 @@ private:
   int next_j_ = 0;
 };
 
+class PetalAnimation2 : public Animation {
+ public:
+  PetalAnimation2(I2cBus& bus) : pm_(bus) {}
+  void Init() {
+    pm_.Init();
+  }
+
+  void Update(int count) override {
+    int step = count < 0 ? -1 : 1;
+    for (size_t i = 0; i < abs(count); i++) {
+      pm_.LedState(next_j_, next_i_, set_or_clear_);
+      next_j_ += step;
+      if (next_j_ < 0 || next_j_ >= kMaxj) {
+        next_i_ += step;
+        next_j_ = next_j_ < 0 ? kMaxj - 1 : 0;
+      }
+      if (next_i_ < 0 || next_i_ >= kMaxi) {
+        next_i_ = next_i_ < 0 ? kMaxi - 1 : 0;
+        set_or_clear_ = !set_or_clear_;
+      }
+    }
+  }
+  void HandlePlaybackDirectionChanged(bool play_forwards) override {
+    set_or_clear_ = !set_or_clear_;
+  }
+
+private:
+  static constexpr size_t kMaxi = 7;
+  static constexpr size_t kMaxj = 8;
+  PetalMatrix pm_;
+  bool set_or_clear_ = true;
+  int next_i_ = 0;
+  int next_j_ = 0;
+};
+
 uint32_t PositionToFrameTime(uint8_t position) {
   constexpr size_t kMinFrameTime = 5000;
   constexpr size_t kMaxFrameTime = 100000;
@@ -54,6 +89,7 @@ uint32_t PositionToFrameTime(uint8_t position) {
 
 int main() {
   constexpr size_t kDefaultFrameTiming = 10000;
+  constexpr uint64_t kAnimationSwitchInterval = 10000000;
 
   stdio_init_all();
   I2cBus i2c_bus0(i2c0, 0, 1);
@@ -62,9 +98,23 @@ int main() {
   i2c_bus1.Init();
 
   // Application stuff.
-  PetalAnimation pa(i2c_bus0, kDefaultFrameTiming);
+  Animator pa(kDefaultFrameTiming);
+  PetalAnimation1 pa1(i2c_bus0);
+  PetalAnimation2 pa2(i2c_bus0);
+
+  std::array<Animation*, 2> animations{
+    &pa1,
+    &pa2,
+  };
+
   TouchWheel tw(i2c_bus1);
-  pa.Init();
+  size_t current_animation = 0;
+  pa.SetAnimation(animations[current_animation]);
+
+  // TODO: Init should go somewhere else.
+  pa1.Init();
+
+  uint64_t last_animation_change = time_us_64();
   while (true) {
     pa.Advance();
 
@@ -80,6 +130,13 @@ int main() {
     } else if (wheel_pos == 0) {
       pa.PlayForwards(true);
       pa.SetFrameTime(kDefaultFrameTiming);
+
+      uint64_t current_time = time_us_64();
+      if (current_time > last_animation_change + kAnimationSwitchInterval) {
+        last_animation_change = current_time;
+        current_animation = (current_animation + 1) % animations.size();
+        pa.SetAnimation(animations[current_animation]);
+      }
     }
   }
 }
--- public/animation.h
+++ public/animation.h
@@ -6,11 +6,17 @@
 
 class Animation {
  public:
-  Animation(uint32_t frame_timing_us) : frame_timing_us_(frame_timing_us) {}
+  virtual void Update(int step_count) = 0;
+  virtual void HandlePlaybackDirectionChanged(bool play_forwards) {}
+};
+
+class Animator {
+ public:
+  Animator(uint32_t frame_timing_us) : frame_timing_us_(frame_timing_us) {}
 
   void PlayForwards(bool play_forwards) {
-    if (play_forwards != playback_forwards_) {
-      HandlePlaybackDirectionChanged(play_forwards);
+    if (play_forwards != playback_forwards_ && animation_ != nullptr) {
+      animation_->HandlePlaybackDirectionChanged(play_forwards);
     }
     playback_forwards_ = play_forwards;
   }
@@ -21,16 +27,20 @@ class Animation {
     int steps = (time - last_update_us_)/frame_timing_us_;
     if (steps) {
       steps = playback_forwards_ ? steps : -1*steps;
-      Update(steps);
+      if (animation_ != nullptr) {
+        animation_->Update(steps);
+      }
       last_update_us_ = time;
     }
   }
 
-  void SetFrameTime(uint32_t frame_time) { frame_timing_us_ = frame_time; }
-  virtual void Update(int count) = 0;
- private:
-  virtual void HandlePlaybackDirectionChanged(bool play_forwards) {}
+  void SetAnimation(Animation* animation) {
+    animation_ = animation;
+  }
 
+  void SetFrameTime(uint32_t frame_time) { frame_timing_us_ = frame_time; }
+ private:
+  Animation* animation_ = nullptr;
   bool playback_forwards_ = true;
   uint64_t last_update_us_ = 0;
   uint32_t frame_timing_us_;
