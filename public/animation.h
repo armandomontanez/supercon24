c44689f3687a2f29cb37a7c15b6f5ce9d33bc7e3
Armando Montanez <montanez.armando.l@gmail.com>
2024-11-03 13:04:29 -0800

    Break out animation class into header

---
+++ public/animation.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstdint>
+
+class Animation {
+ public:
+  Animation(uint32_t frame_timing_us) : frame_timing_us_(frame_timing_us) {}
+
+  void PlayForwards(bool play_forwards) {
+    if (play_forwards != playback_forwards_) {
+      HandlePlaybackDirectionChanged(play_forwards);
+    }
+    playback_forwards_ = play_forwards;
+  }
+
+  void Advance() {
+    uint64_t time = time_us_64();
+
+    int steps = (time - last_update_us_)/frame_timing_us_;
+    if (steps) {
+      steps = playback_forwards_ ? steps : -1*steps;
+      Update(steps);
+      last_update_us_ = time;
+    }
+  }
+
+  void SetFrameTime(uint32_t frame_time) { frame_timing_us_ = frame_time; }
+  virtual void Update(int count) = 0;
+ private:
+  virtual void HandlePlaybackDirectionChanged(bool play_forwards) {}
+
+  bool playback_forwards_ = true;
+  uint64_t last_update_us_ = 0;
+  uint32_t frame_timing_us_;
+};
